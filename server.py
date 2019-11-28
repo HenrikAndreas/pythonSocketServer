4827970bb80119b3fcde33719625b1795aa4135c
Henrik <henrikbratli@live.no>
2019-11-28 23:59:37 +0100

    small changes made

--- server.py
+++ server.py
@@ -1,14 +1,42 @@
-#Python Socket Server
 from socket import *
+import sys
 import select
 
 class Server(object):
     def __init__(self):
         self._IP = "localhost"
         self._PORT = 1234
-        self._serverSocket = (AF_INET, SOCK_STREAM)
-	    
-    def serverLoop(self):
+        self._HEADERLENGTH = 1024
+        self._serverSocket = socket(AF_INET, SOCK_STREAM)
+        self._serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self._serverSocket.bind((self._IP, self._PORT))
+        self._serverSocket.listen()
+        self._socketsList = [self._serverSocket]
+        self._clients = {}
+    
+    def getIP(self):
+        return self._IP
+
+    def getPort(self):
+        return self._PORT
+    
+    def _recieveMessage(self, clientSocket):
         pass
 
+    def serverLoop(self):
+        while True:
+            readSockets, writeSockets, exceptionSockets = select.select(self._socketsList, [], self._socketsList)
+
+            for notifiedSocket in readSockets:
+
+                if notifiedSocket == self._serverSocket:
+                    clientSocket, clientAdress = self._serverSocket.accept()
+                    
+                    username = None #FIX (If username exists - login : else - register)
+                    password = None #FIX
+                    
+                    
+
+                    
 server = Server()
+# server.serverLoop()
