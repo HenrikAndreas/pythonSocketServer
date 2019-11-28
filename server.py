6c64cf3ae8c82e6d78b8a6c87f59ea10ac7fc17e
Henrik Andreas Bratli <henrik@localhost.localdomain>
2019-11-28 23:46:30 +0100

    Added serverLoop

--- server.py
+++ server.py
@@ -6,5 +6,8 @@ class Server(object):
         self._IP = "localhost"
         self._PORT = 1234
         self._serverSocket = (AF_INET, SOCK_STREAM)
+    
+    def serverLoop(self):
+        pass
 
 server = Server()
